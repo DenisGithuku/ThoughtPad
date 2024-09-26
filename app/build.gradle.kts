ad4542a4cde3e4428bde5022bd929b41953a13e5
githukudenis <githukudenis@gmail.com>
2024-09-26 13:46:37 +0300

    Prepare for second release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.gitsoft.thoughtpad"
         minSdk = 23
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
