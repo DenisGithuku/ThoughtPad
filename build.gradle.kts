b39da5f715c558d23e93b30f82e50b9f77c77143
githukudenis <githukudenis@gmail.com>
2024-09-27 08:29:24 +0300

    Add spotless, jacoco and ktlint.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     dependencies {
@@ -10,4 +12,74 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.hilt.android.gradle) apply false
+    alias(libs.plugins.com.diffplug.spotless) apply false
+}
+
+subprojects {
+    apply {
+        plugin("com.diffplug.spotless")
+        plugin("jacoco")
+    }
+
+    configure<SpotlessExtension> {
+        val lintVersion = "0.49.0"
+
+        kotlin {
+            target("**/*.kt")
+            ktlint(lintVersion)
+                .editorConfigOverride(
+                    mapOf(
+                        "ktlint_standard_package-name" to "disabled",
+                        "ij_kotlin_allow_trailing_comma" to "false",
+                        "ij_kotlin_allow_trailing_comma_on_call_site" to "false",
+                    ),
+                )
+            ktfmt().googleStyle()
+            licenseHeaderFile("${project.rootProject.projectDir}/license-header.txt")
+            trimTrailingWhitespace()
+            indentWithTabs(2)
+            indentWithSpaces(4)
+
+        }
+
+        kotlinGradle {
+            target("*.gradle.kts")
+            ktlint(lintVersion)
+                .editorConfigOverride(
+                    mapOf(
+                        "ij_kotlin_allow_trailing_comma" to "false",
+                        "ij_kotlin_allow_trailing_comma_on_call_site" to "false",
+                    )
+                )
+            ktfmt().googleStyle()
+            indentWithTabs(2)
+            indentWithSpaces(4)
+
+        }
+
+        format("xml") {
+            target("**/*.xml")
+            indentWithSpaces()
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+
+    }
+
+    configurations.all {
+        resolutionStrategy {
+            eachDependency {
+                when (requested.group) {
+                    "org.jacoco" -> useVersion("0.8.11")
+                }
+            }
+        }
+    }
+
+    tasks.withType<Test> {
+        configure<JacocoTaskExtension> {
+            isIncludeNoLocationClasses = true
+            excludes = listOf("jdk.internal.*", "**org.hl7*")
+        }
+    }
 }
