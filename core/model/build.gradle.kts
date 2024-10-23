41fde4ad8c27b04cd7f0fe8fc6b04dfc79d5ed6f
githukudenis <githukudenis@gmail.com>
2024-10-23 18:44:34 +0300

    Add flag to disable schema error in CI builds.

--- core/model/build.gradle.kts
+++ core/model/build.gradle.kts
@@ -6,3 +6,12 @@ plugins {
 android { namespace = "com.gitsoft.thoughtpad.core.model" }
 
 dependencies {}
+
+val isCiBuild = System.getenv("CI")?.toBoolean() ?: false
+
+// Disables KSP generating room schema when running on CI
+ksp {
+    if (isCiBuild) {
+        arg("ksp.schema", "false")
+    }
+}
