da6d1a8ad55e6aead7240482ba9512eb91357fb5
githukudenis <githukudenis@gmail.com>
2024-11-03 16:55:57 +0300

    Add note viewmodel tests

---
+++ core/data/src/main/kotlin/core/gitsoft/thoughtpad/core/data/repository/fakes/FakeUserPrefsRepository.kt
@@ -0,0 +1,40 @@
+
+/*
+* Copyright 2024 Denis Githuku
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package core.gitsoft.thoughtpad.core.data.repository.fakes
+
+import com.gitsoft.thoughtpad.core.model.ThemeConfig
+import com.gitsoft.thoughtpad.core.model.UserPreferences
+import core.gitsoft.thoughtpad.core.data.repository.UserPrefsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class FakeUserPrefsRepository : UserPrefsRepository {
+
+    private var _userPrefs =
+        UserPreferences(themeConfig = ThemeConfig.DARK, isNotificationPermissionsGranted = true)
+
+    override val userPrefs: Flow<UserPreferences>
+        get() = flow { emit(_userPrefs) }
+
+    override suspend fun updateTheme(themeConfig: ThemeConfig) {
+        _userPrefs = _userPrefs.copy(themeConfig = themeConfig)
+    }
+
+    override suspend fun updateNotificationPermission(isGranted: Boolean) {
+        _userPrefs = _userPrefs.copy(isNotificationPermissionsGranted = isGranted)
+    }
+}
