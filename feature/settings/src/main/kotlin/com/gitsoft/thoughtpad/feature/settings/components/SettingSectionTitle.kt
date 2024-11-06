8776c8bfbf9ab2231fe2db8ab60537beb68f0e6a
githukudenis <githukudenis@gmail.com>
2024-11-06 07:37:25 +0300

    Create settings section title

---
+++ feature/settings/src/main/kotlin/com/gitsoft/thoughtpad/feature/settings/components/SettingSectionTitle.kt
@@ -0,0 +1,47 @@
+
+/*
+* Copyright 2024 Denis Githuku
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* https://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.gitsoft.thoughtpad.feature.settings.components
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.gitsoft.thoughtpad.core.toga.components.text.TogaSmallTitle
+import com.gitsoft.thoughtpad.feature.settings.R
+
+@Composable
+fun SettingSectionTitle(@StringRes title: Int) {
+    Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+        TogaSmallTitle(text = stringResource(title))
+        Spacer(modifier = Modifier.height(8.dp))
+        HorizontalDivider(thickness = 0.8.dp)
+    }
+}
+
+@Preview
+@Composable
+private fun SettingSectionTitlePrev() {
+    SettingSectionTitle(R.string.settings)
+}
